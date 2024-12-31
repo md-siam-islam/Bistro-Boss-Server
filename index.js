2c4b41f4efa4285b70bbfe0e34cc0febb1121af5
md-siam-islam <mdsiamislam663@gmail.com>
2024-12-31 20:18:07 +0600

    server side thake data load done

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    const dataBase = client.db('bistroBoss')
+    const menuCollection = dataBase.collection('menu')
+
+    app.get('/menu' ,async(req,res) => {
+        
+        const result = await menuCollection.find().toArray()
+        res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
